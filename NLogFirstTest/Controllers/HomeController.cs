b35f6b480800d019aee52eedc6e1995943a93491
WillB01 <wb.william@outlook.com>
2018-03-08 10:15:30 +0100

    initial commit

---
+++ NLogFirstTest/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NLogFirstTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Index page says hello");
+            return View();
+        }
+    }
+}
--- NLogFirstTest/Program.cs
+++ NLogFirstTest/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.Internal.Networking;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
 
 namespace NLogFirstTest
 {
@@ -14,12 +17,29 @@ namespace NLogFirstTest
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var logger = LogManager.LoadConfiguration("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
+                BuildWebHost(args).Run();
+            }
+            catch
+            {
+                //NLog: catch setup errors
+                logger.Error("Stopped program becouse of exception");
+                throw;
+            }
+            
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                }).UseNLog()
                 .Build();
     }
 }
